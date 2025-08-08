911f2b234613a1fc2372f9c0be7ad88e3f174926
Alka Yadav <yalka834@gmail.com>
2025-08-08 16:06:21 +0000

    ASCII_value

---
+++ ASCII.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ASCII{
+    public static void main(String[] args){
+      for(int i=97;i<=122;i++){
+            System.out.println(i+ " "+(char)i);
+      }
+    }
+}
