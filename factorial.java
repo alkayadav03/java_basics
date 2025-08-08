7f6b3577b3e69c664994e19dd8af9dcfe93883ce
Alka Yadav <yalka834@gmail.com>
2025-08-08 15:58:56 +0000

    factorial

---
+++ factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class factorial{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the no.");
+        int n=sc.nextInt();
+        int factorial=1;
+      for(int i=1;i<=n;i++){
+          factorial=factorial*i;
+      }
+        System.out.print("factorial:"+factorial);
+    }
+}
